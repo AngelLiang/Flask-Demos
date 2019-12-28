b69b396c1a46457a1f46b54fa74aa05c20d66eb4
AngelLiang <pl01665077@163.com>
2019-12-28 18:56:46 +0800

    Updated CO04-flask-postgresql-stream-demo/create-scipt.sql

--- CO04-flask-postgresql-stream-demo/create-scipt.sql
+++ CO04-flask-postgresql-stream-demo/create-scipt.sql
@@ -6,6 +6,8 @@ CREATE TABLE message (
     content TEXT NOT NULL
 );
 
+-- 因为要序列化这些消息让客户端能处理，
+-- 所以用到了 PostgreSQL 的 LISTEN 和 NOTIFY 功能。
 -- notify_on_insert 函数
 CREATE OR REPLACE FUNCTION notify_on_insert() RETURNS trigger AS $$
 BEGIN
@@ -16,5 +18,6 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- notify_on_insert 的触发器
+-- 对 message 表每一次 INSERT 操作都发送通知消息
 CREATE TRIGGER notify_on_message_insert AFTER INSERT ON message
 FOR EACH ROW EXECUTE PROCEDURE notify_on_insert();
