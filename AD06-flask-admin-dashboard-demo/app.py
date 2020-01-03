96e461cf585946f973855397ffe75596fce1fa38
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-01-03 17:40:52 +0800

    Updated AD06-flask-admin-dashboard-demo

--- AD06-flask-admin-dashboard-demo/app.py
+++ AD06-flask-admin-dashboard-demo/app.py
@@ -1,16 +1,18 @@
 from flask import Flask
-from flask_admin import Admin, AdminIndexView as _AdminIndexView
+from flask_admin import Admin, AdminIndexView
 from flask_admin import expose
 
 
-class AdminIndexView(_AdminIndexView):
+class DashoboardView(AdminIndexView):
     @expose('/')
     def index(self):
         self.name = 'Dashboard'
         return self.render('admin/dashboard.html')
 
 
-admin = Admin(template_mode='bootstrap3', index_view=AdminIndexView())
+admin = Admin(template_mode='bootstrap3', index_view=DashoboardView(
+    menu_icon_type='glyph',
+    menu_icon_value='glyphicon-dashboard'))
 
 app = Flask(__name__)
 app.config['FLASK_ADMIN_FLUID_LAYOUT'] = True
