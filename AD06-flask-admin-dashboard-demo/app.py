999fac8bbb834ee079444922c2a5de63e8df30d9
AngelLiang <pl01665077@163.com>
2020-01-11 19:54:40 +0800

    Updated AD06-flask-admin-dashboard-demo

--- AD06-flask-admin-dashboard-demo/app.py
+++ AD06-flask-admin-dashboard-demo/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_admin import Admin, AdminIndexView
 from flask_admin import expose
-from flask_admin.base import MenuLink
+from flask_admin.menu import MenuLink
 
 
 class DashoboardView(AdminIndexView):
@@ -12,16 +12,18 @@ class DashoboardView(AdminIndexView):
 
 
 admin = Admin(template_mode='bootstrap3', index_view=DashoboardView(
-    menu_icon_type='glyph',
-    menu_icon_value='glyphicon-dashboard'))
+    menu_icon_type='glyph', menu_icon_value='glyphicon-dashboard'))
 
-
+# 以下 link 会向左靠齐
 admin.add_link(MenuLink(name='Back Home', url='/admin', category='Links'))
 admin.add_link(MenuLink(name='Flask-Demos',
                         url='https://github.com/AngelLiang/Flask-Demos',
                         category='Links'))
 admin.add_link(MenuLink(
     name='Baidu', url='http://www.baidu.com/', category='Links'))
+# 以下 link 会向右靠齐
+admin.add_link(MenuLink(name='Logout', url='#'))
+
 
 app = Flask(__name__)
 app.config['FLASK_ADMIN_FLUID_LAYOUT'] = True
