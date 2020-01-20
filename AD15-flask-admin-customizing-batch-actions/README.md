1243b21c10812de48b54f6f006effe990e72cf48
AngelLiang <pl01665077@163.com>
2020-01-20 20:15:46 +0800

    Added AD15-flask-admin-customizing-batch-actions

---
+++ AD15-flask-admin-customizing-batch-actions/README.md
@@ -0,0 +1,17 @@
+# flask-admin 客制化批量处理操作
+
+
+## 快速开始
+
+    pipenv install
+    pipenv run flask run
+
+
+## 示例图
+
+![admin](screenshot/admin.png)
+
+
+## 相关连接
+
+- https://flask-admin.readthedocs.io/en/latest/advanced/#customizing-batch-actions
---
+++ AD15-flask-admin-customizing-batch-actions/app.py
@@ -0,0 +1,83 @@
+from flask import Flask, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.babel import ngettext, gettext
+from flask_admin.contrib.sqla import ModelView
+from flask_admin.actions import action
+
+
+db = SQLAlchemy()
+admin = Admin(template_mode='bootstrap3')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '123456790'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+admin.init_app(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(128))
+
+    def approve(self):
+        return True
+
+
+class UserModelView(ModelView):
+    column_list = ('id', 'name', 'username')
+    column_default_sort = 'id'
+
+    can_delete = False
+
+    @action('approve', 'Approve', 'Are you sure you want to approve selected users?')
+    def action_approve(self, ids):
+        try:
+            query = User.query.filter(User.id.in_(ids))
+
+            count = 0
+            for user in query.all():
+                if user.approve():
+                    count += 1
+
+            flash(ngettext('User was successfully approved.',
+                           '%(count)s users were successfully approved.',
+                           count,
+                           count=count))
+        except Exception as ex:
+            if not self.handle_view_exception(ex):
+                raise
+
+            flash(gettext('Failed to approve users. %(error)s', error=str(ex)), 'error')
+
+
+admin.add_view(UserModelView(User, db.session))
+
+
+def initdata(user_count=50):
+    db.drop_all()
+    db.create_all()
+
+    users = []
+    for i in range(user_count):
+        user = User(name=f'name{i+1}', username=f'user{i+1}')
+        users.append(user)
+    db.session.add_all(users)
+    db.session.commit()
+
+
+@app.route('/')
+def index():
+    return '<a href="/admin/">Click me to get to Admin!</a>'
+
+
+@app.before_first_request
+def init_data():
+    initdata()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
