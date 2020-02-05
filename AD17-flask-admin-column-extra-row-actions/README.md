f6d85c869b5f315108d4d2efac093f9b8cc219d4
AngelLiang <pl01665077@163.com>
2020-02-05 14:43:24 +0800

    Added AD17-flask-admin-column-extra-row-actions

---
+++ AD17-flask-admin-column-extra-row-actions/README.md
@@ -0,0 +1,11 @@
+# 给 flask-admin 每个 row 添加额外的 action
+
+## 快速开始
+
+    pipenv install
+    pipenv run flask run
+
+
+## 示例图
+
+![user](screenshot/user.png)
---
+++ AD17-flask-admin-column-extra-row-actions/app.py
@@ -0,0 +1,84 @@
+from flask import Flask, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin, expose
+from flask_admin.contrib.sqla import ModelView
+from flask_admin.model.template import EndpointLinkRowAction, LinkRowAction
+
+db = SQLAlchemy()
+admin = Admin(template_mode='bootstrap3')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '123456790'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+admin.init_app(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    username = db.Column(db.String(80))
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = password
+
+
+class UserModelView(ModelView):
+    column_list = ('id', 'name', 'username')
+    column_default_sort = 'id'
+
+    can_delete = False
+
+    # 给 flask-admin 每个 row 数据添加额外的 action
+    column_extra_row_actions = [
+        EndpointLinkRowAction('glyphicon glyphicon-envelope', '.approve'),
+        LinkRowAction('glyphicon glyphicon-off', ''),
+    ]
+
+    @expose('/user/approve')
+    def approve(self):
+        # TODO:
+        return redirect(url_for('.index_view'))
+
+
+admin.add_view(UserModelView(User, db.session))
+
+
+def initdb(user_count=50):
+    db.drop_all()
+    db.create_all()
+
+    from faker import Faker
+    fake = Faker('zh_CN')
+
+    user = User(username='admin', name='admin')
+    user.set_password('admin')
+    db.session.add(user)
+    db.session.commit()
+
+    users = []
+    for i in range(user_count):
+        user = User(
+            name=fake.name(),
+            username=fake.profile()['username'],
+        )
+        user.set_password('123456')
+        users.append(user)
+    db.session.add_all(users)
+    db.session.commit()
+
+
+@app.before_first_request
+def init_data():
+    initdb()
+
+
+@app.route('/')
+def index():
+    return '<a href="/admin/">Click me to go to Admin!</a>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
