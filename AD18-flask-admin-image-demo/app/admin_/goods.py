8cc77b28a68d5c7498c811c02ee93a6b57257ec3
AngelLiang <pl01665077@163.com>
2020-02-06 21:41:20 +0800

    format AD18-flask-admin-image-demo

--- AD18-flask-admin-image-demo/app/admin_/goods.py
+++ AD18-flask-admin-image-demo/app/admin_/goods.py
@@ -1,9 +1,6 @@
 from flask_admin.contrib.sqla import ModelView
 
-from app.models import Image
-from .image_view.image import CustomImageUploadField
 from .image_view.image_field import ImageField
-from .image_view.image_inline_form import ImageInlineModelForm
 from .image_view.images_formatter import images_formatter
 
 
@@ -14,16 +11,9 @@ class GoodsModelView(ModelView):
     column_formatters = {
         'images': images_formatter,
     }
-
     column_details_list = ('name', 'images',)
 
     form_columns = ('name', 'images',)
-
-    # inline_models = [ImageInlineModelForm(Image), ]
-
-    # Alternative way to contribute field is to override it completely.
-    # In this case, Flask-Admin won't attempt to merge various parameters for the field.
     form_extra_fields = {
-        # 'images': CustomImageUploadField(),
-        'images': ImageField(),  # 表单额外的字段
+        'images': ImageField(),
     }
