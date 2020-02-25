e9c761b2901e8ce7b580cb1fc18d7d0d61fcd73d
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-02-25 11:55:41 +0800

    Updated AD27-flask-admin-export

--- AD27-flask-admin-export/app.py
+++ AD27-flask-admin-export/app.py
@@ -19,11 +19,28 @@ admin.init_app(app)
 # models
 
 
+post_tags_table = db.Table(
+    'post_tags', db.Model.metadata,
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128))
     text = db.Column(db.Text)
     date = db.Column(db.Date)
+
+    tags = db.relationship('Tag', secondary=post_tags_table)
 
     def __repr__(self):
         return f'{self.title}'
@@ -35,7 +52,41 @@ class Post(db.Model):
 
 class PostModelView(ModelView):
 
+    column_list = ('title', 'tags', 'date')
+
     can_export = True
+    column_export_list = ('id', 'title', 'tags', 'text', 'date')
+    """
+        Collection of the field names included in the export.
+        If set to `None`, will get them from the model.
+    """
+
+    column_export_exclude_list = None
+    """
+        Collection of fields excluded from the export.
+    """
+
+    column_formatters_export = None
+    """
+        Dictionary of list view column formatters to be used for export.
+
+        Defaults to column_formatters when set to None.
+
+        Functions the same way as column_formatters except
+        that macros are not supported.
+    """
+
+    column_type_formatters_export = None
+    """
+        Dictionary of value type formatters to be used in the export.
+
+        By default, two types are formatted:
+
+        1. ``None`` will be displayed as an empty string
+        2. ``list`` will be joined using ', '
+
+        Functions the same way as column_type_formatters.
+    """
 
     # 使用 xls 或 xlsx 还需安装 openpyxl
     # `pip install tablib[xls,xlsx]` or `pip install openpyxl`
@@ -58,7 +109,8 @@ admin.add_view(PostModelView(Post, db.session))
 # initdb
 
 
-def initdb(post_count=100):
+def initdb(post_count=100, tag_count=10):
+    import random
     from faker import Faker
 
     fake = Faker()
@@ -66,11 +118,25 @@ def initdb(post_count=100):
     db.drop_all()
     db.create_all()
 
+    tags = []
+    for i in range(tag_count):
+        tag = Tag(
+            name=fake.word(),
+        )
+        tags.append(tag)
+    db.session.add_all(tags)
+    db.session.commit()
+
     posts = []
     for i in range(post_count):
         post = Post(
             title=fake.sentence(),
             date=fake.past_date(),
+            tags=list(set([
+                Tag.query.get(random.randint(1, Tag.query.count())),
+                Tag.query.get(random.randint(1, Tag.query.count())),
+                Tag.query.get(random.randint(1, Tag.query.count())),
+            ]))
         )
         posts.append(post)
     db.session.add_all(posts)
